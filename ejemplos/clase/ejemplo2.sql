1c730114bf9a409a92009c98bf57b5052a6eef37
Diego Fernández Criado <diegofercri25@gmail.com>
2024-04-22 20:29:20 +0200

    x

---
+++ ejemplos/clase/ejemplo2.sql
@@ -0,0 +1,14 @@
+SET SERVEROUTPUT ON;
+
+DECLARE
+    V_NUMBER NUMBER(5) := 0;
+BEGIN
+    LOOP
+        V_NUMBER := V_NUMBER + 1;
+        DBMS_OUTPUT.PUT_LINE(V_NUMBER);
+        IF V_NUMBER = 10 THEN
+            EXIT;
+        END IF;
+    END LOOP;
+END;
+/
--- ejemplos/ejemplo2.sql
+++ ejemplos/ejemplo2.sql
@@ -1,12 +1,38 @@
 DECLARE
-    V_NUMBER NUMBER(5) := 0;
+ /* Comienzo de la sección declarativa */
+    V_STUDENTID NUMBER(5) := 10000;
+    V_FIRSTNAME VARCHAR2(20);
 BEGIN
-    LOOP
-        V_NUMBER := V_NUMBER + 1;
-        DBMS_OUTPUT.PUT_LINE(V_NUMBER);
-        IF V_NUMBER = 10 THEN
-            EXIT;
-        END IF;
-    END LOOP;
+ /* Comienzo de la sección ejecutable */
+ -- Obtener el nombre del estudiante con identificador 10000
+    SELECT
+        NAME INTO V_FIRSTNAME
+    FROM
+        STUDENTS S
+    WHERE
+        S.ID = V_STUDENTID;
+ -- Comienzo de un bloque anidado que contiene solo una sección ejecutable
+    BEGIN -- Es buena idea identar los bloques anidados
+        INSERT INTO LOG_TABLE (
+            INFO
+        ) VALUES (
+            'Hola desde un bloque anidado!'
+        );
+    END;
+EXCEPTION
+ /* Comienzo de la sección de manejo de excepciones */
+    WHEN NO_DATA_FOUND THEN
+ -- Comienzo de un bloque anidado, que el mismo contiene una sección ejecutable y manejo de excepciones
+        BEGIN
+ -- Tratamiento de la condición de error
+        INSERT INTO LOG_TABLE (
+            INFO
+        ) VALUES (
+            'El estudiante 10,000 no existe'
+        );
+        EXCEPTION
+    WHEN OTHERS THEN
+ -- Algo ha fallado en la inserción en la tabla log_table
+        DBMS_OUTPUT.PUT_LINE('Error al insertar en log_table!');
 END;
-/
+END;
