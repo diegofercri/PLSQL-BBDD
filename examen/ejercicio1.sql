1d25a4664578d6f32ef401c9c4c963368235ab49
Diego Fernández Criado <diegofercri25@gmail.com>
2024-04-24 11:21:20 +0200

    EJER 1

--- examen/ejercicio1.sql
+++ examen/ejercicio1.sql
@@ -1,28 +1,36 @@
 SET SERVEROUTPUT ON;
 
 DECLARE
-    V_OFICIO_ID OFICIO.OFICIO_ID%TYPE;
-    V_OFICIO OFICIO.OFICIO%TYPE;
+    V_OFICIO_ID   OFICIO.OFICIO_ID%TYPE;
+    V_OFICIO      OFICIO.OFICIO%TYPE;
     V_EMPLEADO_ID EMPLEADO.EMPLEADO_ID%TYPE;
-    
     CURSOR C_OFICIO IS
-    SELECT OFICIO_ID, OFICIO
-    FROM OFICIO
-    ORDER BY OFICIO;
+    SELECT
+        OFICIO_ID,
+        OFICIO
+    FROM
+        OFICIO
+    ORDER BY
+        OFICIO;
 BEGIN
     OPEN C_OFICIO;
     LOOP
         FETCH C_OFICIO INTO V_OFICIO_ID, V_OFICIO;
         EXIT WHEN C_OFICIO%NOTFOUND;
         DBMS_OUTPUT.PUT_LINE('OFICIO: ' || V_OFICIO);
-        
+
         FOR EMPLEADO_ID IN (
-            SELECT EMPLEADO
-            FROM OFICIOEMPLEADO
-            WHERE OFICIO = V_OFICIO_ID
-            ORDER BY EMPLEADO DESC
+            SELECT
+                EMPLEADO
+            FROM
+                OFICIOEMPLEADO
+            WHERE
+                OFICIO = V_OFICIO_ID
+            ORDER BY
+                EMPLEADO DESC
         ) LOOP
-            DBMS_OUTPUT.PUT_LINE('   EMPLEADO_ID: ' || V_EMPLEADO_ID);
+            DBMS_OUTPUT.PUT_LINE('   EMPLEADO_ID: '
+                                 || V_EMPLEADO_ID);
         END LOOP;
     END LOOP;
 
