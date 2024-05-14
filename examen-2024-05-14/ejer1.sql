b650a502847109dc6fd12a9f13e5bd0041a6f171
Diego Fernández Criado <diegofercri25@gmail.com>
2024-05-14 10:36:39 +0200

    EXAMEN procedimientos funciones y paquetes

--- examen-2024-05-14/ejer1.sql
+++ examen-2024-05-14/ejer1.sql
@@ -1,19 +1,27 @@
+SET SERVEROUTPUT ON;
 /* En esta ocasión no vamos a controlar que se produzcan excepciones */
 /* NOTA: SI ejecutamos varias veces el programa debe insertar correctamente */
-declare
-  v_Oficio Oficio%rowtype;
-  procedure print(p_Oficio Oficio%rowtype) is
-    begin
-      dbms_output.put_line('id='||p_Oficio.numoficio||' ofi='||trim(p_Oficio.oficio)||' desc='||trim(p_Oficio.descripcion));
-    end;
-begin
-  /* Crear un nuevo Oficio, no pasamos el id por lo que lo debe crear
-  el procedimiento usando una secuencia */
-  v_Oficio.oficio:='pasota';
-  v_Oficio.descripcion:='vamos, que pasa de todo como algunos de clase';
-  print(v_oficio); -- Antes sale sin id
-  addOficio(v_Oficio);
-  print(v_oficio); -- Ahora tiene que salir con el id
-end;
+DECLARE
+    v_Oficio OFICIO%ROWTYPE;
+    PROCEDURE print(p_Oficio OFICIO%ROWTYPE) IS
+    BEGIN
+        dbms_output.put_line('id='||p_Oficio.NUMOFICIO||' ofi='||trim(p_Oficio.OFICIO)||' desc='||trim(p_Oficio.DESCRIPCION));
+    END;
+  
+    PROCEDURE addOficio(p_Oficio IN OUT OFICIO%rowtype) IS
+    BEGIN
+        p_Oficio.numoficio := sec_oficio.NEXTVAL;
+        INSERT INTO OFICIO (NUMOFICIO, OFICIO, DESCRIPCION) VALUES (p_Oficio.numoficio, p_Oficio.oficio, p_Oficio.descripcion);
+    END;
+
+BEGIN
+    /* Crear un nuevo Oficio, no pasamos el id por lo que lo debe crear
+    el procedimiento usando una secuencia */
+    v_Oficio.OFICIO:='pasota';
+    v_Oficio.DESCRIPCION:='vamos, que pasa de todo como algunos de clase';
+    print(v_Oficio); -- Antes sale sin id
+    addOficio(v_Oficio);
+    print(v_Oficio); -- Ahora tiene que salir con el id
+END;
 /
-rollback;
+ROLLBACK;
---
+++ examen-2024-05-14/sequence.sql
@@ -0,0 +1,3 @@
+-- Crear la secuencia
+DROP SEQUENCE sec_oficio;
+CREATE SEQUENCE sec_oficio START WITH 7 INCREMENT BY 1;
